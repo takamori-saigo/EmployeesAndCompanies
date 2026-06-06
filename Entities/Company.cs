e79801dc6362e42dd0e0e9a69a103ab30875914d
takamori-saigo <98211881+takamori-saigo@users.noreply.github.com>
2026-06-06 13:53:40 +0500

    done domain - layer

---
+++ Entities/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+
+public class Company
+{
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Name is required")]
+    [MaxLength(60, ErrorMessage = "Name cannot be longer than 60 characters")]
+    public string? Name { get; set; }
+    [Required(ErrorMessage = "Address is required")]
+    [MaxLength(60, ErrorMessage = "Address cannot be longer than 60 characters")]
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public ICollection<Employee>? Employees { get; set; }
+}
