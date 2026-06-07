f9b9eadc06f6f9d7f24e32848cbf5c0fc17871d5
takamori-saigo <98211881+takamori-saigo@users.noreply.github.com>
2026-06-07 11:53:13 +0500

    Add date in db

--- Repository/Migrations/20260607064520_AddDateToBd.Designer.cs
+++ Repository/Migrations/20260607064520_AddDateToBd.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Repository;
@@ -11,9 +12,11 @@ using Repository;
 namespace RepositoryContext.Migrations
 {
     [DbContext(typeof(Repository.RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20260607064520_AddDateToBd")]
+    partial class AddDateToBd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
